d4521b203cdf0f4c0d5cadff8cd796ca47f5204e
VinitKumarGupta <guptavinit973@gmail.com>
2026-03-27 17:48:08 +0530

    Fixed: hardcoded port to dynamic version

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080, () => {
-    console.log("The server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`The server is listening to port ${port}`);
 });
