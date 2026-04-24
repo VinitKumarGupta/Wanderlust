d55b18d175defc071cdf1abd0bba62690edfdcf8
VinitKumarGupta <guptavinit973@gmail.com>
2026-04-24 19:37:05 +0530

    Fix dynamic CORS

--- app.js
+++ app.js
@@ -24,13 +24,10 @@ const flash = require("connect-flash");
 
 const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
-// *============== CORS (dev only) ==============* //
+// *============== CORS ==============* //
 app.use(
     cors({
-        origin:
-            process.env.NODE_ENV === "production"
-                ? "https://wanderlust-murex-omega.vercel.app/listings"
-                : "http://localhost:5173",
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
         credentials: true,
     }),
 );
